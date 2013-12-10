73aef37434e8a315da96461d1fadc065574d21b7
Crazy Penguin <crazy.frozen.penguin@gmail.com>
2013-12-10 00:24:08 -0500

    Initial Gitlab Mailer Web Hook application.

---
+++ README.md
@@ -0,0 +1,16 @@
+Gitlab Mail
+-----------
+
+This small application receives updates from Gitlab and pushes updates desired email addresses.
+
+
+REST API:
+
+POST /gitlab/:project
+
+The above end-point expects Gitlab POST json as explained here: 
+http://git.aliens-lyon.fr/help/web_hooks
+
+Quick Test
+curl -X POST -d @test-data.json --header "Content-Type: application/json" http://localhost:9000/gitlab/my_gitlab_project
+
---
+++ app/controllers/Mailer.scala
@@ -0,0 +1,34 @@
+package controllers
+
+import mail.{Mail, send}
+import play.api.mvc._
+import play.api.libs.json._
+
+/**
+ * Created with IntelliJ IDEA.
+ * Date: 12/9/13
+ * Time: 9:19 PM
+ */
+object Mailer extends Controller {
+
+  def fromGitlab(app: String) = Action(parse.json) {
+    request => {
+      Option(app) match {
+        case None => BadRequest(Json.obj("status" -> "KO", "message" -> "Application identifier required"))
+        case _ => {
+          System.out.println("Request mailer app: " + app)
+          System.out.println("Received body: " + request.body)
+
+          send a new Mail (
+            from = ("gitlabmailer@test.com", "Gitlab Mailer"),
+            to = Seq("bmrosantos@gmail.com"),
+            subject = "Import stuff",
+            message = request.body.toString()
+          )
+
+          Ok
+        }
+      }
+    }
+  }
+}
